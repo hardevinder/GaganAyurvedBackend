9a3b94e1d872cde1d90e7e9efc6920eb1219f1e5
hardevinder22g <hardevinder22g@gmail.com>
2025-09-14 10:42:21 +0530

    Updated

--- src/types/external.d.ts
+++ src/types/external.d.ts
@@ -1,1 +1,12 @@
-declare module "nodemailer";
+// src/types/external.d.ts
+
+// Fixes for external libraries without official type definitions
+declare module "pdfkit" {
+  const PDFDocument: any;
+  export = PDFDocument;
+}
+
+declare module "nodemailer" {
+  const nodemailer: any;
+  export = nodemailer;
+}
